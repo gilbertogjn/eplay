eddb1ac07bf70ecb2c6f4d10f3f30bf27b303d93
Gilberto Garcia <gilbertogjn@gmail.com>
2024-03-04 11:08:03 -0400

    add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,17 @@
+import { Container } from './styles'
+
 const Footer = () => (
-    <div>
-        <div className="container">
-            <div>
-                <h4>Categorias</h4>
-            </div>
-        </div>
+  <Container>
+    <div className="container">
+      <div>
+        <h4>Categorias</h4>
+      </div>
+      <div>
+        <h4>Acesso rápido</h4>
+      </div>
+      <p>2023 - &copy; E-PLAY - Todos os direitos reservados</p>
     </div>
+  </Container>
 )
+
+export default Footer
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import { colors } from '../../styles'
+
+export const Container = styled.footer`
+  background-color: ${colors.grey};
+  padding: 32px 0;
+`
