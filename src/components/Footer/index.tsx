b0b455a2db5083e22b4ac3aefc55f087f976f918
Gilberto Garcia <gilbertogjn@gmail.com>
2024-03-14 15:55:18 -0400

    feat: add product page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,51 @@
-import { Container } from './styles'
+import { Container, FooterSection, Link, Links, SectionTitle } from './styles'
+
+const currentYear = new Date().getFullYear()
 
 const Footer = () => (
   <Container>
     <div className="container">
-      <div>
-        <h4>Categorias</h4>
-      </div>
-      <div>
-        <h4>Acesso rápido</h4>
-      </div>
-      <p>2023 - &copy; E-PLAY - Todos os direitos reservados</p>
+      <FooterSection>
+        <SectionTitle>Categorias</SectionTitle>
+        <Links>
+          <li>
+            <Link>RPG</Link>
+          </li>
+          <li>
+            <Link>Ação</Link>
+          </li>
+          <li>
+            <Link>Aventura</Link>
+          </li>
+          <li>
+            <Link>Esportes</Link>
+          </li>
+          <li>
+            <Link>Estratégia</Link>
+          </li>
+          <li>
+            <Link>FPS</Link>
+          </li>
+        </Links>
+      </FooterSection>
+      <FooterSection>
+        <SectionTitle>Acesso rápido</SectionTitle>
+        <Links>
+          <li>
+            <Link>Novidades</Link>
+          </li>
+          <li>
+            <Link>Promoções</Link>
+          </li>
+          <li>
+            <Link>Aventura</Link>
+          </li>
+          <li>
+            <Link>Em breve</Link>
+          </li>
+        </Links>
+      </FooterSection>
+      <p>{currentYear} - &copy; E-PLAY - Todos os direitos reservados</p>
     </div>
   </Container>
 )
